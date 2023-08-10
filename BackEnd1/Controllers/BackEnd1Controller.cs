5842524076afb26317048fe785e5d62d85aaf310
mbeekman <mbeekman@gmail.com>
2023-08-10 00:04:47 -0500

    Fleshing out service

---
+++ BackEnd1/Controllers/BackEnd1Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd1;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BackEnd1Controller : ControllerBase
+{
+    private const int BackEnd1Result = 42;
+
+    [HttpGet]
+    public int GetBackEndResult()
+    {
+        Thread.Sleep(500);  //arbitrary sleep per requirements
+        return BackEnd1Result;
+    }
+}
